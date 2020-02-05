386f3ad5f200b8ec9701bdc0d63f846bc6d4432b
RavinaSa <58072044+RavinaSa@users.noreply.github.com>
2020-02-05 15:50:11 +0000

    stuff
    only some changes to my folder, testing stuff

---
+++ GameStart/Assets/Ravina/RSScripts/RS_JK_Raycast.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class RS_JK_Raycast : MonoBehaviour
+{
+    private string RaycastObject;
+    private GameObject Object;
+
+    private Vector3 pickupPos;
+    private Vector3 oldPos;
+
+    private bool isPlayerHolding;
+
+    [SerializeField]
+    private GameObject Player;
+
+    private float RotSpeed = 40;
+
+
+
+    //Ravina Stuff
+    public GameObject joints;
+    private Animator jointsanim;
+    public GameObject surface;
+    private Animator surfaceanim;
+    private GameObject Silhoutte;
+    private Animator SilhoutteAnim;
+
+    Color transparent = new Color(0f, 0f, 0f, 0f);
+
+    private void Start()
+    {
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            int layerMask = 1 << 8;
+            layerMask = ~layerMask;
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask) && isPlayerHolding == false)
+            {
+                RaycastObject = hit.collider.gameObject.name;
+                Object = GameObject.Find(RaycastObject);
+                if (Object.tag == "Pickup")
+                {
+                    Debug.Log("Hit pickup with raycast");
+                    isPlayerHolding = true;
+                    FirstPersonController.isPlayerHolding = true;
+                    oldPos = Object.transform.position;
+                    Object.GetComponent<Rigidbody>().isKinematic = true;
+                    Object.GetComponent<BoxCollider>().enabled = false;
+                }
+
+                 if (Object.tag == "Person")
+                {
+                    Debug.Log("Hit person with raycast");
+                    SilhoutteAnim = Object.GetComponent<Animator>();
+                    SilhoutteAnim.Play("Throw");
+                }
+                else
+                {
+                    Debug.Log("Did not hit pickup");
+                }
+            }
+            else if (isPlayerHolding == true)
+            {
+                Object.transform.position = oldPos;
+                isPlayerHolding = false;
+                FirstPersonController.isPlayerHolding = false;
+                Object.GetComponent<BoxCollider>().enabled = true;
+            }
+        }
+        //RotateHeldObject();
+        MoveHeldObject();
+    }
+
+    void RotateHeldObject()
+    {
+        if (isPlayerHolding == true)
+        {
+            Object.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * Time.deltaTime * RotSpeed);
+        }
+    }
+    void MoveHeldObject()
+    {
+        if (isPlayerHolding == true)
+        {
+            pickupPos = transform.position + transform.forward;
+            Object.transform.position = pickupPos;
+        }
+    }
+}
+
---
+++ GameStart/Assets/Ravina/RSScripts/RS_SilhoutteFade.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RS_SilhoutteFade : MonoBehaviour
+{
+    public GameObject joints;
+    private Animator jointsanim;
+    public GameObject surface;
+    private Animator surfaceanim;
+    private GameObject Silhoutte;
+    private Animator SilhoutteAnim;
+    
+    Color transparent = new Color(0f, 0f, 0f, 0f);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Silhoutte = this.gameObject;
+        SilhoutteAnim = Silhoutte.GetComponent<Animator>();
+        SilhoutteAnim.Play("Throw");
+
+        jointsanim = joints.GetComponent<Animator>();
+        surfaceanim = surface.GetComponent<Animator>();
+
+        jointsanim.Play("FadeJoints");
+        surfaceanim.Play("FadeSurface");
+
+        
+
+
+        //if (SilhoutteAnim.GetCurrentAnimatorStateInfo(0).IsName("Throw"))
+       
+        {
+           // var jointsRenderer = joints.GetComponent<Renderer>();
+           // jointsRenderer.material.SetColor("_Color", transparent);
+
+            //var surfaceRenderer = surface.GetComponent<Renderer>();
+           // surfaceRenderer.material.SetColor("_Color", transparent);
+           // Debug.Log( "Yes, Im invisible");
+        }
+
+
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void fadeOut ()
+    {
+
+        
+
+    }
+}
