1d1330bba6f3550cd4c2035cf24520fdeed639fd
AbiSanders7 <abi.sanders777@gmail.com>
2020-02-07 11:33:40 +0000

    Pick Up Colour Change
    change colour when you pick up an object. absolutely wild.

---
+++ GameStart/Assets/Abi/Scripts/PickupColour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupColour : MonoBehaviour
+{
+    public enum colours
+    {
+        Blank,
+        //MAIN
+        Red,
+        Yellow,
+        Green,
+        Blue,
+        Purple,
+        Brown,
+        //OPTIONAL
+        Orange,
+        Pink,
+
+
+    }
+    public colours UnlockedColour;
+}
