3a248365588623c5d35b57265bb2f5309f8f2c38
JNKemp <58072078+JNKemp@users.noreply.github.com>
2020-02-13 21:51:11 +0000

    Portals

--- GameStart/Assets/JordanK/PortalTP.cs
+++ GameStart/Assets/JordanK/PortalTP.cs
@@ -19,7 +19,7 @@ public class PortalTP : MonoBehaviour
             if (dotProduct < 0f)
             {
                 float rotationDifference = Quaternion.Angle(transform.rotation, reciever.rotation);
-                //rotationDifference += 180f;
+                rotationDifference += 180f;
                 player.transform.Rotate(Vector3.up, rotationDifference);
 
                 Vector3 positionOffset = Quaternion.Euler(0, rotationDifference, 0) * portalToPlayer;
