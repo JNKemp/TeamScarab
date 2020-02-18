4ab5b4f30de5cbf7d8aa25f034ea910d3048b14f
RavinaSa <58072044+RavinaSa@users.noreply.github.com>
2020-02-18 18:43:09 +0000

    UI Added
    Settings screen added

---
+++ GameStart/Assets/Ravina/RSScripts/RS_SettingsChangeTab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RS_SettingsChangeTab : MonoBehaviour
+{
+    private GameObject ControlsImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControlsImage = GameObject.Find("ControlsTabImage");
+    }
+
+    public void switchtabs()
+    {
+        ControlsImage.SetActive(false);
+    }
+}
