48e1b39feacd06648d4c45f4484781ddedb60405
AbiSanders7 <abi.sanders777@gmail.com>
2020-02-23 11:45:13 +0000

    Door Appear and Ravina's UI

---
+++ GameStart/Assets/Ravina/RSScripts/RS_ControlsSwitchTab.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RS_ControlsSwitchTab : MonoBehaviour
+{
+    private GameObject ControlImage;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ControlImage = GameObject.Find("ControlsTabImage");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void activateTab()
+    {
+        ControlImage.SetActive(true);
+    }
+}
