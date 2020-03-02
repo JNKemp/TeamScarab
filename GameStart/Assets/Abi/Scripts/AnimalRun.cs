46550aca3c223290a5deb6c63fa94e337723a9f7
AbiSanders7 <abi.sanders777@gmail.com>
2020-03-02 12:03:30 +0000

    Deer runs away when you walk into a trigger

---
+++ GameStart/Assets/Abi/Scripts/AnimalRun.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalRun : MonoBehaviour
+{
+    private GameObject go_PC;
+
+    [SerializeField]
+    private GameObject go_animal;
+    private Animator an_animal;
+    [SerializeField]
+    private GameObject go_controller;
+    private Animator an_controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        go_PC = GameObject.Find("FPSController");
+        an_animal = go_animal.GetComponent<Animator>();
+        an_controller = go_controller.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == go_PC)
+        {
+            an_animal.SetBool("isRunning", true);
+            an_controller.SetBool("isTriggered", true);
+        }
+    }
+}
