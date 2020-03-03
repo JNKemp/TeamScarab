331b3344ffabe270b969062c618d3f9698f85687
RavinaSa <58072044+RavinaSa@users.noreply.github.com>
2020-03-03 11:22:28 +0000

    Forestmorefleshedout
    Did some more stuff

--- GameStart/Assets/Abi/Scripts/ChangeColourOnTrigger.cs
+++ GameStart/Assets/Abi/Scripts/ChangeColourOnTrigger.cs
@@ -9,6 +9,7 @@ public class ChangeColourOnTrigger : MonoBehaviour
     //Set int the IDE whether or not the object should be destroyed after the colour is unlocked
     [SerializeField]
     private bool DestroyObjectAfter;
+    private GameObject player;
 
     public enum colours
     {
@@ -34,23 +35,27 @@ public class ChangeColourOnTrigger : MonoBehaviour
         go_colourManager = GameObject.Find("Colour Manager"); //Finds the object called Colour Manager
         Debug.Assert(go_colourManager, "There is no Colour Manager in the scene! That's wild please put one in our whole game revolves around colour! :,)");
         str_colourtounlock = ColourToUnlock.ToString();//Converts the chosen colour to a string
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (ColourToUnlock != colours.Blank)
+        if (other.gameObject == player)
         {
-            go_colourManager.GetComponent<ColourManager>().str_unlockedColours.Add(str_colourtounlock); //Adds the chosen colour to the list of unlocked colours in the Colour Manager.
+            if (ColourToUnlock != colours.Blank)
+            {
+                go_colourManager.GetComponent<ColourManager>().str_unlockedColours.Add(str_colourtounlock); //Adds the chosen colour to the list of unlocked colours in the Colour Manager.
+            }
         }
         else
         {
             Debug.LogAssertion("You need to assign a colour to unlock! It is currently set as Blank! That's not what you want! There's a dropdown list of all colours on the object this script is on. :)");
         }
-        
-        if (DestroyObjectAfter)
-        {
-            Destroy(gameObject); //Destroys the gameobject if it has been set to
-        }
+
+            if (DestroyObjectAfter)
+            {
+                Destroy(gameObject); //Destroys the gameobject if it has been set to
+            }
         
     }
 }
---
+++ GameStart/Assets/Ravina/RSScripts/RS_AppleFall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RS_AppleFall : MonoBehaviour
+
+    
+{
+    private GameObject player;
+    public GameObject Apple;
+    private Rigidbody appleRigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        appleRigid = Apple.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            appleRigid.isKinematic = false;
+        }
+    }
+}
