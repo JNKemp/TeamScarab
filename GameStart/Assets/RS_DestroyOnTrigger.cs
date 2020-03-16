a67f4947e82740e3097a49992685c3a2a15bfe5a
RavinaSa <58072044+RavinaSa@users.noreply.github.com>
2020-03-16 19:16:31 +0000

    Edited
    Beach edit, final room, ekg sound and paintings

---
+++ GameStart/Assets/RS_DestroyOnTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RS_DestroyOnTrigger : MonoBehaviour
+{
+    public GameObject activator;
+    public GameObject asset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == activator)
+        {
+            asset.SetActive(false);
+        }
+    }
+}
